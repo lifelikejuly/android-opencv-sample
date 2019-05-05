8a92a1525972855865d5b14b7ceeaa9a8f8113fc
haocanyu <yuhc@tuya.com>
2019-05-05 19:47:22 +0800

    do: add permission

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ OpenCV official demo to AS
 
 ## Project
 https://opencv.org/releases.html
+
+# changeLog
+
+* 官方源码移植到AS上重新打包
+* 增加权限动态申请
--- app/src/main/java/com/julyyu/opencv/sample/MainAcitivity.java
+++ app/src/main/java/com/julyyu/opencv/sample/MainAcitivity.java
@@ -1,13 +1,20 @@
 package com.julyyu.opencv.sample;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.julyyu.opencv.demo.imageblur.ImageBlurActivity;
 import com.julyyu.opencv.sample.cameracalibration.CameraCalibrationActivity;
@@ -18,6 +25,9 @@ import com.julyyu.opencv.sample.puzzle15.Puzzle15Activity;
 import com.julyyu.opencv.sample.tutorial1.Tutorial1Activity;
 import com.julyyu.opencv.sample.tutorial2.Tutorial2Activity;
 import com.julyyu.opencv.sample.tutorial3.Tutorial3Activity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * author : JulyYu
@@ -37,6 +47,11 @@ public class MainAcitivity extends AppCompatActivity implements AdapterView.OnIt
             "imageblur"
     };
 
+    private String[] permissions ={
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +61,9 @@ public class MainAcitivity extends AppCompatActivity implements AdapterView.OnIt
 
         listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, presidents));
         listView.setOnItemClickListener(this);
+
+        requestPermission(permissions,1);
+
     }
 
     @Override
@@ -83,5 +101,37 @@ public class MainAcitivity extends AppCompatActivity implements AdapterView.OnIt
                 break;
         }
 
+    }
+
+    private boolean requestPermission(String[] permissions, int requestCode) {
+        //判断当前Activity是否已经获得了该权限
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+        List<String> DeniedPermission = new ArrayList<>();
+        for(String permission : permissions){
+            if (ContextCompat.checkSelfPermission(this,
+                    permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                DeniedPermission.add(permission);
+            }
+        }
+        if(DeniedPermission.size() > 0){
+            ActivityCompat.requestPermissions(this,(String[])DeniedPermission.toArray(),
+                    requestCode);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for(int i =0;i < grantResults.length ;i++){
+            if(grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this,"NO PERMISSION",Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
     }
 }
